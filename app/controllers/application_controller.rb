aacdcdfae7e3acf755b7fa9b856dc140fdb672b8
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2020-01-07 22:15:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,19 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
 
+    get '/' do
+    erb :'../views/super_hero'
+    end
+
+    post '/teams' do
+      @team = Team.new(params[:team])
+
+      params[:team][:members].each do |details|
+        SuperHero.new(details)
+      end
+      @super_heros = SuperHero.all
+
+      erb :'../views/team'
+
+    end
 end
